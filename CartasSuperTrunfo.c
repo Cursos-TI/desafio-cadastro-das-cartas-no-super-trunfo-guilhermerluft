4fa7a64b6990981c7dd9cbf88b596fad00e5cec4
guilhermerluft <guilhermerluft@gmail.com>
2025-02-15 20:02:33 -0300

    Adiciona resolução do desafio avançado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,92 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    //Atributos da carta A
+    char nomeCartaA[50];
+    float populacaoCartaA;
+    float areaCartaA;
+    float pibCartaA;
+    int qtdPontosTuristicosCartaA;
+
+    //Atributos da carta B
+    char nomeCartaB[50];
+    float populacaoCartaB;
+    float areaCartaB;
+    float pibCartaB;
+    int qtdPontosTuristicosCartaB;
+
+    while(true) {
+        printf("\n=+=+=+ CADASTRO DA CARTA A +=+=+=\n");
+
+        printf("Digite o nome do país: ");
+        scanf("%s", &nomeCartaA);
+
+        printf("Digite a população de %s:", nomeCartaA);
+        scanf("%f", &populacaoCartaA);
+
+        printf("Digite a área de %s:", nomeCartaA);
+        scanf("%f", &areaCartaA);
+
+        printf("Digite o pib de %s:", nomeCartaA);
+        scanf("%f", &pibCartaA);
+
+        printf("Digite a quantidade de pontos turísticos  de %s:", nomeCartaA);
+        scanf("%d", &qtdPontosTuristicosCartaA);
+
+        printf("\n=+=+=+ CADASTRO DA CARTA B +=+=+=\n");
+
+        printf("Digite o nome do país: ");
+        scanf("%s", &nomeCartaB);
+
+        printf("Digite a população de %s:", nomeCartaB);
+        scanf("%f", &populacaoCartaB);
+
+        printf("Digite a área de %s:", nomeCartaB);
+        scanf("%f", &areaCartaB);
+
+        printf("Digite o PIB de %s:", nomeCartaB);
+        scanf("%f", &pibCartaB);
+
+        printf("Digite a quantidade de pontos turísticos de %s:", nomeCartaB);
+        scanf("%d", &qtdPontosTuristicosCartaB);
+
+        if(populacaoCartaA < populacaoCartaB) {
+            printf("\nVencedor para população: %s", nomeCartaA);
+        } else {
+            printf("\nVencedor para população: %s", nomeCartaB);
+        }        
+
+        if(areaCartaA > areaCartaB) {
+            printf("\nVencedor para área: %s", nomeCartaA);
+        } else {
+            printf("\nVencedor para área: %s", nomeCartaB);
+        }
+
+        if(pibCartaA > pibCartaB) {
+            printf("\nVencedor para PIB: %s", nomeCartaA);
+        } else {
+            printf("\nVencedor para PIB: %s", nomeCartaB);
+        }
+
+        if(qtdPontosTuristicosCartaA > qtdPontosTuristicosCartaB) {
+            printf("\nVencedor para quantidade de pontos turísticos: %s", nomeCartaA);
+        } else {
+            printf("\nVencedor para quantidade de pontos turístico: %s", nomeCartaB);
+        }
+
+        printf("\n\nDeseja fazer outra comparação?");
+        printf("\n[1] Sim");
+        printf("\n[2] Não\n");
+        int resultado = scanf("%d", &resultado);
+        printf("%d", resultado);
+        if(resultado != 1) {
+            break;
+        } else {
+            printf("Encerrando aplicação...");
+        }
+    }
 
     return 0;
 }
